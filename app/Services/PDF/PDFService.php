f4d5eee62c50aa26f5bc43756b75fd34143e3f82
Emel Islamoski <emel_storm@hotmail.com>
2025-07-12 15:56:46 +0200

    Add kt1 update and couple of fixes

--- app/Services/PDF/PDFService.php
+++ app/Services/PDF/PDFService.php
@@ -41,8 +41,7 @@ class PDFService implements PDFServiceInterface
 
         $pdf = Pdf::loadView('pdfs.forms.form_mt1', [
             'form_data' => $form,
-        ])
-            ->setPaper('A5', 'landscape');
+        ]);
 //            ->setPaper([0, 0, 535.24, 304.33]);
 
         return $pdf->download('mt1_'.$form->custom_id.'_'.$form->date_time.'.pdf');
